5f84f3aabb6d1f58beb751faf136ac491ad9ebaf
Collin Schlager <schlagercollin@gmail.com>
2022-04-22 15:32:08 -0700

    Initial work for hmm_smooth

--- ssm_jax/hmm/messages.py
+++ ssm_jax/hmm/messages.py
@@ -45,3 +45,58 @@ def hmm_filter(hmm, inputs, data):
     carry = (0.0, hmm.initial_dist(inputs[0]))
     (ll, _), filtered_probs = lax.scan(_step, carry, np.arange(T))
     return ll, filtered_probs
+
+
+def hmm_backwards(hmm, inputs, data):
+    """_summary_
+    
+    Notes:
+        beta_t[j]   = p(x_{t+1:T} | z_t = j)
+        beta_tm1[j] = p(x_{t:T}   | z_{t-1} = j)
+
+    Args:
+        hmm (_type_): _description_
+        inputs (_type_): _description_
+        data (_type_): _description_
+
+    Returns:
+        _type_: _description_
+    """
+    T = len(data)
+    
+    def _step(carry, t):
+        log_normalizer, beta_t = carry
+        
+        # Get parameters and inputs for time index t
+        A = hmm.transition_matrix(t, inputs[t])
+        ll = hmm.log_likelihood(t, inputs[t], data[t])
+        
+        ll_max = ll.max()
+        beta_tm1 = A @ (beta_t * np.exp(ll - ll_max))
+        
+        # Renormalize
+        norm = beta_tm1.sum()
+        beta_tm1 /= norm
+        
+        # Update the log log_normalizer
+        log_normalizer += np.log(norm) + ll_max
+        
+        return (log_normalizer, beta_tm1), beta_tm1
+    
+    carry = (0.0, np.ones(hmm.n_states))
+    (ll, _), backwards_messages = lax.scan(_step, carry, np.arange(T)[::-1])
+    return ll, backwards_messages
+
+
+def hmm_smooth(hmm, inputs, data):
+    
+    ll, filtered_probs = hmm_filter(hmm, inputs, data)
+    _, betas = hmm_backwards(hmm, inputs, data)
+    
+    # Compute E[z_t] for t = 1, ..., T
+    # p_zt1T := p(z_t = j | x_{1:T})
+    p_zt1T = filtered_probs * betas
+    norm = p_zt1T.sum(axis=1)
+    p_zt1T /= norm
+    
+    return p_zt1T, ll
