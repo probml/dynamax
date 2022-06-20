ca7d4621c2bdd8c5b5e9ad228243d0650bfc1467
gileshd <gileshd@googlemail.com>
2022-06-20 17:33:58 +0100

    Update docstrings and variable names.
    Make variables consistent with east-coast covention.

--- ssm_jax/lgssm/inference.py
+++ ssm_jax/lgssm/inference.py
@@ -28,15 +28,15 @@ class LGSSMPosterior:
 
     Attributes:
             marginal_loglik: marginal log likelihood of the data
-            filtered_means: (T,K) array,
+            filtered_means: (T,D_hid) array,
                 E[x_t | y_{1:t}, u_{1:t}].
-            filtered_covariances: (T,K,K) array,
+            filtered_covariances: (T,D_hid,D_hid) array,
                 Cov[x_t | y_{1:t}, u_{1:t}].
-            smoothed_means: (T,K) array,
+            smoothed_means: (T,D_hid) array,
                 E[x_t | y_{1:T}, u_{1:T}].
-            smoothed_covs: (T,K,K) array of smoothed marginal covariances,
+            smoothed_covs: (T,D_hid,D_hid) array of smoothed marginal covariances,
                 Cov[x_t | y_{1:T}, u_{1:T}].
-            smoothed_cross: (T-1, K, K) array of smoothed cross products,
+            smoothed_cross: (T-1, D_hid, D_hid) array of smoothed cross products,
                 E[x_t x_{t+1}^T | y_{1:T}, u_{1:T}].
     """
     marginal_loglik: chex.Scalar = None
@@ -50,37 +50,64 @@ class LGSSMPosterior:
 # Helper functions
 _get_params = lambda x, dim, t: x[t] if x.ndim == dim+1 else x
 
-def _predict(m, S, A, B, b, Q, u):
+def _predict(m, S, F, B, b, Q, u):
     """Predict next mean and covariance under a linear Gaussian model
 
-        p(x_{t+1}) = \int N(x_t | m, S) N(x_{t+1} | Ax_t + Bu + b, Q)
-                    = N(x_{t+1} | Am + Bu, A S A^T + Q)
+        p(x_{t+1}) = \int N(x_t | m, S) N(x_{t+1} | Fx_t + Bu + b, Q)
+                    = N(x_{t+1} | Fm + Bu, F S F^T + Q)
+
+    Args:
+        m (D_hid,): prior mean.
+        S (D_hid,D_hid): prior covariance.
+        F (D_hid,D_hid): dynamics matrix.
+        B (D_hid,D_in): dynamics input matrix.
+        u (D_in,): inputs.
+        Q (D_hid,D_hid): dynamics covariance matrix.
+        b (D_hid,): dynamics bias.
+
+    Returns:
+        mu_pred (D_hid,): predicted mean.
+        Sigma_pred (D_hid,D_hid): predicted covariance.
     """
-    mu_pred = A @ m + B @ u + b
-    Sigma_pred = A @ S @ A.T + Q
+    mu_pred = F @ m + B @ u + b
+    Sigma_pred = F @ S @ F.T + Q
     return mu_pred, Sigma_pred
 
 
-def _condition_on(m, S, C, D, d, R, u, y):
+def _condition_on(m, S, H, D, d, R, u, y):
     """Condition a Gaussian potential on a new linear Gaussian observation
       p(x_t | y_t, u_t, y_{1:t-1}, u_{1:t-1})
         propto p(x_t | y_{1:t-1}, u_{1:t-1}) p(y_t | x_t, u_t)
-        = N(x_t | m, S) N(y_t | C_t x_t + D_t u_t + d_t, R_t)
+        = N(x_t | m, S) N(y_t | H_t x_t + D_t u_t + d_t, R_t)
         = N(x_t | mm, SS)
     where
-    mm = m + K*(y - yhat) = mu_cond
-    yhat = H*m + D*u + d
-    K = S * H' * (R + H * S * H')^{-1}
-    L = I - K*H
-    SS = L * S * L' + K * R * K' = Sigma_cond
+        mm = m + K*(y - yhat) = mu_cond
+        yhat = H*m + D*u + d
+        K = S * H' * (R + H * S * H')^{-1}
+        L = I - K*H
+        SS = L * S * L' + K * R * K' = Sigma_cond
     **Note! This can be done more efficiently when R is diagonal.**
+
+   Args:
+        m (D_hid,): prior mean.
+        S (D_hid,D_hid): prior covariance.
+        H (D_obs,D_hid): emission matrix.
+        D (D_obs,D_in): emission input weights.
+        u (D_in,): inputs.
+        d (D_obs,): emission bias.
+        R (D_obs,D_obs): emission covariance matrix.
+        y (D_obs,): observation.
+
+    Returns:
+        mu_pred (D_hid,): predicted mean.
+        Sigma_pred (D_hid,D_hid): predicted covariance.
     """
     # Compute the Kalman gain
-    K = jnp.linalg.solve(R + C @ S @ C.T, C @ S).T
+    K = jnp.linalg.solve(R + H @ S @ H.T, H @ S).T
     dim = m.shape[-1]
-    ImKC = jnp.eye(dim) - K @ C
-    Sigma_cond = ImKC @ S @ ImKC.T + K @ R @ K.T
-    mu_cond = m + K @ (y - D @ u - d - C @ m)
+    ImKH = jnp.eye(dim) - K @ H
+    Sigma_cond = ImKH @ S @ ImKH.T + K @ R @ K.T
+    mu_cond = m + K @ (y - D @ u - d - H @ m)
     return mu_cond, Sigma_cond
 
 
@@ -90,14 +117,14 @@ def lgssm_filter(params, emissions, inputs=None):
 
     Args:
         params: an LGSSMParams instance (or object with the same fields)
-        inputs: array of length T containing inputs.
-        emissions: array (T,D) of data.
+        emissions (T,D_hid): array of observations.
+        inputs (T,D_in): array of inputs.
 
     Returns:
         filtered_posterior: LGSSMPosterior instance containing,
-            marginal_log_lik
-            filtered_means
-            filtered_covariances
+            marginal_log_lik 
+            filtered_means (T, D_hid)
+            filtered_covariances (T, D_hid, D_hid)
     """
     num_timesteps = len(emissions)
     inputs = jnp.zeros((num_timesteps, 0)) if inputs is None else inputs
@@ -106,11 +133,11 @@ def lgssm_filter(params, emissions, inputs=None):
         ll, pred_mean, pred_cov = carry
 
         # Shorthand: get parameters and inputs for time index t
-        A = _get_params(params.dynamics_matrix, 2, t)
+        F = _get_params(params.dynamics_matrix, 2, t)
         B = _get_params(params.dynamics_input_weights, 2, t)
         b = _get_params(params.dynamics_bias, 1, t)
         Q = _get_params(params.dynamics_covariance, 2, t)
-        C = _get_params(params.emission_matrix, 2, t)
+        H = _get_params(params.emission_matrix, 2, t)
         D = _get_params(params.emission_input_weights, 2, t)
         d = _get_params(params.emission_bias, 1, t)
         R = _get_params(params.emission_covariance, 2, t)
@@ -118,16 +145,16 @@ def lgssm_filter(params, emissions, inputs=None):
         y = emissions[t]
 
         # Update the log likelihood
-        ll += MVN(C @ pred_mean + D @ u + d,
-                  C @ pred_cov @ C.T + R).log_prob(y)
+        ll += MVN(H @ pred_mean + D @ u + d,
+                  H @ pred_cov @ H.T + R).log_prob(y)
 
         # Condition on this emission
         filtered_mean, filtered_cov = _condition_on(
-            pred_mean, pred_cov, C, D, d, R, u, y)
+            pred_mean, pred_cov, H, D, d, R, u, y)
 
         # Predict the next state
         pred_mean, pred_cov = _predict(
-            filtered_mean, filtered_cov, A, B, b, Q, u)
+            filtered_mean, filtered_cov, F, B, b, Q, u)
 
         return (ll, pred_mean, pred_cov), (filtered_mean, filtered_cov)
 
@@ -146,13 +173,13 @@ def lgssm_posterior_sample(rng, params, emissions, inputs=None):
 
     Args:
         rng: jax.random.PRNGKey.
-        params: an LGSSMParams instance (or object with the same fields).
-        inputs: array of length T containing inputs.
-        emissions: array (T,D) of data.
+        params: an LGSSMParams instance (or object with the same fields)
+        emissions (T,D_hid): array of observations.
+        inputs (T,D_in): array of inputs.
 
     Returns:
-        ll: marginal log likelihood of the data
-        states: array (T,K) of samples from the posterior distribution on latent states.
+        ll: marginal log likelihood of the observations.
+        states (T,D_hid): samples from the posterior distribution on latent states.
     """
     num_timesteps = len(emissions)
     inputs = jnp.zeros((num_timesteps, 0)) if inputs is None else inputs
@@ -167,7 +194,7 @@ def lgssm_posterior_sample(rng, params, emissions, inputs=None):
         rng, filtered_mean, filtered_cov, t = args
 
         # Shorthand: get parameters and inputs for time index t
-        A = _get_params(params.dynamics_matrix, 2, t)
+        F = _get_params(params.dynamics_matrix, 2, t)
         B = _get_params(params.dynamics_input_weights, 2, t)
         b = _get_params(params.dynamics_bias, 1, t)
         Q = _get_params(params.dynamics_covariance, 2, t)
@@ -175,7 +202,7 @@ def lgssm_posterior_sample(rng, params, emissions, inputs=None):
 
         # Condition on next state
         smoothed_mean, smoothed_cov = _condition_on(
-            filtered_mean, filtered_cov, A, B, b, Q, u, next_state)
+            filtered_mean, filtered_cov, F, B, b, Q, u, next_state)
         state = MVN(smoothed_mean, smoothed_cov).sample(seed=rng)
         return state, state
 
@@ -199,8 +226,8 @@ def lgssm_smoother(params, emissions, inputs=None):
 
     Args:
         params: an LGSSMParams instance (or object with the same fields)
-        inputs: array of (T,Din) containing inputs.
-        emissions: array (T,Dout) of data.
+        emissions (T,D_hid): array of observations.
+        inputs (T,D_in): array of inputs.
 
     Returns:
         lgssm_posterior: LGSSMPosterior instance containing properites of
@@ -220,7 +247,7 @@ def lgssm_smoother(params, emissions, inputs=None):
         t, filtered_mean, filtered_cov = args
 
         # Shorthand: get parameters and inputs for time index t
-        A = _get_params(params.dynamics_matrix, 2, t)
+        F = _get_params(params.dynamics_matrix, 2, t)
         B = _get_params(params.dynamics_input_weights, 2, t)
         b = _get_params(params.dynamics_bias, 1, t)
         Q = _get_params(params.dynamics_covariance, 2, t)
@@ -228,13 +255,13 @@ def lgssm_smoother(params, emissions, inputs=None):
 
         # This is like the Kalman gain but in reverse
         # See Eq 8.11 of Saarka's "Bayesian Filtering and Smoothing"
-        G = jnp.linalg.solve(Q + A @ filtered_cov @ A.T, A @ filtered_cov).T
+        G = jnp.linalg.solve(Q + F @ filtered_cov @ F.T, F @ filtered_cov).T
 
         # Compute the smoothed mean and covariance
         smoothed_mean = filtered_mean + \
-            G @ (smoothed_mean_next - A @ filtered_mean - B @ u - b)
+            G @ (smoothed_mean_next - F @ filtered_mean - B @ u - b)
         smoothed_cov = filtered_cov + \
-            G @ (smoothed_cov_next - A @ filtered_cov @ A.T - Q) @ G.T
+            G @ (smoothed_cov_next - F @ filtered_cov @ F.T - Q) @ G.T
 
         # Compute the smoothed expectation of x_t x_{t+1}^T
         smoothed_cross = G @ smoothed_cov_next + \
