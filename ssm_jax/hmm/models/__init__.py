e01fe1264208716c133078afbe1e8cc535c36c90
karalleyna <aleynakara98@gmail.com>
2022-07-11 22:07:49 +0000

    Add categorical hmm test

---
+++ ssm_jax/hmm/models/__init__.py
@@ -0,0 +1,4 @@
+from ssm_jax.hmm.models.bernoulli_hmm import BernoulliHMM
+from ssm_jax.hmm.models.categorical_hmm import CategoricalHMM
+from ssm_jax.hmm.models.categorical_logit_hmm import CategoricalLogitHMM
+from ssm_jax.hmm.models.gaussian_hmm import GaussianHMM
