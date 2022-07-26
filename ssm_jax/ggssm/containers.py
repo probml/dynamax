b74989c7303a3b3a21262be00cb8c48bfc42b40d
Peter G. Chang <peter0414@gmail.com>
2022-07-26 07:39:59 -0400

    Add GHKF container for GGF

--- ssm_jax/ggssm/containers.py
+++ ssm_jax/ggssm/containers.py
@@ -1,6 +1,8 @@
 from typing import Callable
-from dataclasses import dataclass
+from dataclasses import dataclass, field
 
+from numpy.polynomial.hermite_e import hermegauss
+from sklearn.utils.extmath import cartesian
 from jax import jacfwd
 from jax import vmap
 import jax.numpy as jnp
@@ -56,22 +58,39 @@ class EKFParams(GGSSMParams):
 
 
 @dataclass
-class UKFParams(GGSSMParams):
+class SigmaPointParams(GGSSMParams):
+    """
+    Lightweight container for sigma point filter/smoother parameters.
+    """
+    def _gaussian_expectation(self, f, m, P):
+        w_mean, _, sigmas = self.compute_weights_and_sigmas(m, P)
+        return jnp.tensordot(w_mean, vmap(f)(sigmas), axes=1)
+
+    def _gaussian_cross_covariance(self, f, g, m, P):
+        _, w_cov, sigmas = self.compute_weights_and_sigmas(m, P)
+        _outer = vmap(lambda x, y: jnp.atleast_2d(x).T @ jnp.atleast_2d(y), 0, 0)
+        f_mean, g_mean = self.gaussian_expectation(f, m, P), self.gaussian_expectation(g, m, P)
+        return jnp.tensordot(w_cov, _outer(vmap(f)(sigmas) - f_mean, vmap(g)(sigmas) - g_mean), axes=1)
+
+
+@dataclass
+class UKFParams(SigmaPointParams):
     """
     Lightweight container for unscented Kalman filter/smoother parameters.
     """
     alpha: chex.Scalar = jnp.sqrt(3)
     beta: chex.Scalar = 2
     kappa: chex.Scalar = 1
+    compute_weights_and_sigmas: Callable = lambda x, y: (0, 0, 0)
     gaussian_expectation: Callable = None
     gaussian_cross_covariance: Callable = None
     
     def __post_init__(self):
-        self.gaussian_expectation = self._gaussian_expectation
-        self.gaussian_cross_covariance = self._gaussian_cross_covariance
+        self.compute_weights_and_sigmas = self._compute_weights_and_sigmas
+        self.gaussian_expectation = super()._gaussian_expectation
+        self.gaussian_cross_covariance = super()._gaussian_cross_covariance
 
-    # Helper function
-    def _compute_sigmas_and_weights(self, m, P):
+    def _compute_weights_and_sigmas(self, m, P):
         n = len(self.initial_mean)
         lamb = self.alpha**2 * (n + self.kappa) - n
         # Compute weights
@@ -84,13 +103,29 @@ class UKFParams(GGSSMParams):
         sigma_minus = jnp.array([m - distances[:, i] for i in range(n)])
         sigmas = jnp.concatenate((jnp.array([m]), sigma_plus, sigma_minus))
         return w_mean, w_cov, sigmas
-    
-    def _gaussian_expectation(self, f, m, P):
-        w_mean, _, sigmas = self._compute_sigmas_and_weights(m, P)
-        return jnp.tensordot(w_mean, vmap(f)(sigmas), axes=1)
 
-    def _gaussian_cross_covariance(self, f, g, m, P):
-        w_mean, w_cov, sigmas = self._compute_sigmas_and_weights(m, P)
-        _outer = vmap(lambda x, y: jnp.atleast_2d(x).T @ jnp.atleast_2d(y), 0, 0)
-        f_mean, g_mean = self.gaussian_expectation(f, m, P), self.gaussian_expectation(g, m, P)
-        return jnp.tensordot(w_cov, _outer(vmap(f)(sigmas) - f_mean, vmap(g)(sigmas) - g_mean), axes=1)
+
+@dataclass
+class GHKFParams(SigmaPointParams):
+    """
+    Lightweight container for Gauss-Hermite Kalman filter/smoother parameters.
+    """
+    order: chex.Scalar = 10
+    compute_weights_and_sigmas: Callable = lambda x, y: (0, 0, 0)
+    gaussian_expectation: Callable = None
+    gaussian_cross_covariance: Callable = None
+    
+    def __post_init__(self):
+        self.compute_weights_and_sigmas = self._compute_weights_and_sigmas
+        self.gaussian_expectation = super()._gaussian_expectation
+        self.gaussian_cross_covariance = super()._gaussian_cross_covariance
+
+    def _compute_weights_and_sigmas(self, m, P):
+        n = len(self.initial_mean)
+        samples_1d, weights_1d = hermegauss(self.order)
+        weights_1d /= weights_1d.sum()
+        print(jnp.array([weights_1d] * n))
+        weights = jnp.prod(cartesian(jnp.array([weights_1d] * n)), axis=1)
+        unit_sigmas = cartesian(jnp.array([samples_1d] * n))
+        sigmas = m + vmap(jnp.matmul, [None, 0], 0)(jnp.linalg.cholesky(P), unit_sigmas)
+        return weights, weights, sigmas
