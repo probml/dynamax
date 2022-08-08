5a155e42277dec7fff57d4f5efd7b5c1ae6e916f
karalleyna <aleynakara98@gmail.com>
2022-08-08 21:15:57 +0000

    Add Create MultivariateNormalHMMs

--- ssm_jax/hmm/models/tests/gaussian_hmm_test.py
+++ ssm_jax/hmm/models/tests/gaussian_hmm_test.py
@@ -1,7 +1,10 @@
 import jax.numpy as jnp
 import jax.random as jr
+import pytest
 from jax import vmap
 from ssm_jax.hmm.models.gaussian_hmm import GaussianHMM
+from ssm_jax.hmm.models.mvn_diag_hmm import DiagonalGaussianHMM
+from ssm_jax.utils import monotonically_increasing
 
 
 def get_random_gaussian_hmm_params(key, num_states, num_emissions):
@@ -11,7 +14,7 @@ def get_random_gaussian_hmm_params(key, num_states, num_emissions):
     transition_matrix = jr.uniform(transition_key, shape=(num_states, num_states))
     transition_matrix = transition_matrix / jnp.sum(initial_probabilities, axis=-1, keepdims=True)
     emission_means = jr.randint(means_key, shape=(num_states, num_emissions), minval=-20.,
-                                    maxval=20).astype(jnp.float32)
+                                maxval=20).astype(jnp.float32)
     emission_covar_sqrts = jr.normal(covs_key, (num_states, num_emissions))
     emission_covars = jnp.einsum('ki, kj->kij', emission_covar_sqrts, emission_covar_sqrts)
     emission_covars += 1e-1 * jnp.eye(num_emissions)
@@ -34,6 +37,7 @@ def test_fit_means(key=jr.PRNGKey(0), num_states=3, num_emissions=3, num_samples
     hmm.emission_covariance_matrices.freeze()
 
     losses = hmm.fit_sgd(batch_emissions)
+
     assert jnp.allclose(hmm.initial_probs.value, initial_probabilities)
     assert jnp.allclose(hmm.transition_matrix.value, transition_matrix)
     assert jnp.allclose(hmm.emission_covariance_matrices.value, emission_covars)
@@ -81,3 +85,29 @@ def test_fit_transition_matrix(key=jr.PRNGKey(0), num_states=3, num_emissions=3,
     assert jnp.allclose(hmm.initial_probs.value, initial_probabilities)
     assert jnp.allclose(hmm.emission_means.value, emission_means)
     assert jnp.allclose(hmm.emission_covariance_matrices.value, emission_covars)
+
+
+class TestGaussianHMMWithDiagonalCovars:
+
+    def setup(self):
+        key = jr.PRNGKey(0)
+        self.num_states = 3
+        self.emission_dim = 3
+        self.true_hmm = DiagonalGaussianHMM.random_initialization(key, self.num_states, self.emission_dim)
+
+    def test_fit(self, key=jr.PRNGKey(0), num_timesteps=100):
+
+        state_sequence, emissions = self.true_hmm.sample(key, num_timesteps)
+        hmm = DiagonalGaussianHMM.random_initialization(key, self.num_states, self.emission_dim)
+
+        lps = hmm.fit_em(emissions[None, ...])
+        assert monotonically_increasing(lps, atol=1)
+
+    def test_filter(self, key=jr.PRNGKey(0), num_timesteps=100):
+        state_sequence, emissions = self.true_hmm.sample(key, num_timesteps)
+
+        hmm = DiagonalGaussianHMM.random_initialization(key, self.num_states, self.emission_dim)
+
+        posteriors = hmm.filter(emissions)
+        assert not jnp.isnan(posteriors.filtered_probs).any()
+        assert jnp.allclose(posteriors.filtered_probs.sum(axis=1), 1.)
---
+++ ssm_jax/hmm/models/tests/mvn_spherical_hmm_test.py
@@ -0,0 +1,31 @@
+import jax.numpy as jnp
+import jax.random as jr
+import pytest
+from ssm_jax.hmm.models.mvn_spherical_hmm import MultivariateNormalSphericalHMM
+from ssm_jax.utils import monotonically_increasing
+
+
+class TestGaussianHMMWithDiagonalCovars:
+
+    def setup(self):
+        key = jr.PRNGKey(0)
+        self.num_states = 3
+        self.emission_dim = 3
+        self.true_hmm = MultivariateNormalSphericalHMM.random_initialization(key, self.num_states, self.emission_dim)
+
+    def test_fit(self, key=jr.PRNGKey(0), num_timesteps=100):
+
+        state_sequence, emissions = self.true_hmm.sample(key, num_timesteps)
+        hmm = MultivariateNormalSphericalHMM.random_initialization(key, self.num_states, self.emission_dim)
+
+        lps = hmm.fit_em(emissions[None, ...])
+        assert monotonically_increasing(lps, atol=1)
+
+    def test_filter(self, key=jr.PRNGKey(0), num_timesteps=100):
+        state_sequence, emissions = self.true_hmm.sample(key, num_timesteps)
+
+        hmm = MultivariateNormalSphericalHMM.random_initialization(key, self.num_states, self.emission_dim)
+
+        posteriors = hmm.filter(emissions)
+        assert not jnp.isnan(posteriors.filtered_probs).any()
+        assert jnp.allclose(posteriors.filtered_probs.sum(axis=1), 1.)
