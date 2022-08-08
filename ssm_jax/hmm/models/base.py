9b8f89aad271b06ec766b50839e4ba77977e56c5
karalleyna <aleynakara98@gmail.com>
2022-08-08 21:13:35 +0000

    Add initial_probs property to posterior dataclass

--- ssm_jax/hmm/models/base.py
+++ ssm_jax/hmm/models/base.py
@@ -1,25 +1,27 @@
 from abc import abstractmethod
+from functools import partial
 
 import jax.numpy as jnp
 import jax.random as jr
-from numpy import roll
 import optax
 import tensorflow_probability.substrates.jax.bijectors as tfb
 import tensorflow_probability.substrates.jax.distributions as tfd
-from jax import vmap
 from jax import jit
 from jax import lax
-from jax import tree_map, tree_leaves
-from functools import partial
-from tqdm.auto import trange
-
+from jax import tree_leaves
+from jax import tree_map
+from jax import vmap
+from numpy import roll
+from ssm_jax.abstractions import SSM
+from ssm_jax.abstractions import Parameter
 from ssm_jax.hmm.inference import compute_transition_probs
 from ssm_jax.hmm.inference import hmm_filter
 from ssm_jax.hmm.inference import hmm_posterior_mode
 from ssm_jax.hmm.inference import hmm_smoother
 from ssm_jax.hmm.inference import hmm_two_filter_smoother
-from ssm_jax.abstractions import SSM, Parameter
-from ssm_jax.optimize import run_sgd, sample_minibatches
+from ssm_jax.optimize import run_sgd
+from ssm_jax.optimize import sample_minibatches
+from tqdm.auto import trange
 
 
 class BaseHMM(SSM):
@@ -64,28 +66,24 @@ class BaseHMM(SSM):
     # Basic inference code
     def marginal_log_prob(self, emissions):
         """Compute log marginal likelihood of observations."""
-        post = hmm_filter(self._compute_initial_probs(),
-                          self._compute_transition_matrices(),
+        post = hmm_filter(self._compute_initial_probs(), self._compute_transition_matrices(),
                           self._compute_conditional_logliks(emissions))
         ll = post.marginal_loglik
         return ll
 
     def most_likely_states(self, emissions):
         """Compute Viterbi path."""
-        return hmm_posterior_mode(self._compute_initial_probs(),
-                                  self._compute_transition_matrices(),
+        return hmm_posterior_mode(self._compute_initial_probs(), self._compute_transition_matrices(),
                                   self._compute_conditional_logliks(emissions))
 
     def filter(self, emissions):
         """Compute filtering distribution."""
-        return hmm_filter(self._compute_initial_probs(),
-                          self._compute_transition_matrices(),
+        return hmm_filter(self._compute_initial_probs(), self._compute_transition_matrices(),
                           self._compute_conditional_logliks(emissions))
 
     def smoother(self, emissions):
         """Compute smoothing distribution."""
-        return hmm_smoother(self._compute_initial_probs(),
-                            self._compute_transition_matrices(),
+        return hmm_smoother(self._compute_initial_probs(), self._compute_transition_matrices(),
                             self._compute_conditional_logliks(emissions))
 
     # Expectation-maximization (EM) code
@@ -93,31 +91,29 @@ class BaseHMM(SSM):
         """The E-step computes expected sufficient statistics under the
         posterior. In the generic case, we simply return the posterior itself.
         """
+
         # TODO: Assert batch_emissions is really batched!
         def _single_e_step(emissions):
             transition_matrices = self._compute_transition_matrices()
-            posterior = hmm_two_filter_smoother(self._compute_initial_probs(),
-                                                transition_matrices,
+            posterior = hmm_two_filter_smoother(self._compute_initial_probs(), transition_matrices,
                                                 self._compute_conditional_logliks(emissions))
-
+            posterior.initial_probs = posterior.smoothed_probs[0]
             # Compute the transition probabilities
-            posterior.trans_probs = compute_transition_probs(
-                transition_matrices, posterior,
-                reduce_sum=(transition_matrices.ndim == 2))
+            posterior.trans_probs = compute_transition_probs(transition_matrices,
+                                                             posterior,
+                                                             reduce_sum=(transition_matrices.ndim == 2))
 
             return posterior
 
         return vmap(_single_e_step)(batch_emissions)
 
-    def m_step(self, batch_emissions, batch_posteriors,
-               optimizer=optax.adam(1e-2),
-               num_mstep_iters=50):
+    def m_step(self, batch_emissions, batch_posteriors, optimizer=optax.adam(1e-2), num_mstep_iters=50):
         """_summary_
-
         Args:
             emissions (_type_): _description_
             posterior (_type_): _description_
         """
+
         def neg_expected_log_joint(params, minibatch):
             minibatch_emissions, minibatch_posteriors = minibatch
             scale = len(batch_emissions) / len(minibatch_emissions)
@@ -136,29 +132,26 @@ class BaseHMM(SSM):
                 return lp
 
             log_prior = self.log_prior()
-            minibatch_lps = vmap(_single_expected_log_joint)(
-                minibatch_emissions, minibatch_posteriors)
+            minibatch_lps = vmap(_single_expected_log_joint)(minibatch_emissions, minibatch_posteriors)
             expected_log_joint = log_prior + minibatch_lps.sum() * scale
             return -expected_log_joint / batch_emissions.size
 
         # Minimize the negative expected log joint with SGD
         params, losses = run_sgd(neg_expected_log_joint,
-                                 self.unconstrained_params,
-                                 (batch_emissions, batch_posteriors),
+                                 self.unconstrained_params, (batch_emissions, batch_posteriors),
                                  optimizer=optimizer,
                                  num_epochs=num_mstep_iters)
         self.unconstrained_params = params
 
     def fit_em(self, batch_emissions, num_iters=50, **kwargs):
         """Fit this HMM with Expectation-Maximization (EM).
-
         Args:
             batch_emissions (_type_): _description_
             num_iters (int, optional): _description_. Defaults to 50.
-
         Returns:
             _type_: _description_
         """
+
         @jit
         def em_step(params):
             self.unconstrained_params = params
@@ -176,23 +169,22 @@ class BaseHMM(SSM):
         self.unconstrained_params = params
         return jnp.array(log_probs)
 
-    def fit_sgd(self,
-                batch_emissions,
-                optimizer=optax.adam(1e-3),
-                batch_size=1,
-                num_epochs=50,
-                shuffle=False,
-                key=jr.PRNGKey(0),
-        ):
+    def fit_sgd(
+            self,
+            batch_emissions,
+            optimizer=optax.adam(1e-3),
+            batch_size=1,
+            num_epochs=50,
+            shuffle=False,
+            key=jr.PRNGKey(0),
+    ):
         """
         Fit this HMM by running SGD on the marginal log likelihood.
-
         Note that batch_emissions is initially of shape (N,T)
         where N is the number of independent sequences and
         T is the length of a sequence. Then, a random susbet with shape (B, T)
         of entire sequence, not time steps, is sampled at each step where B is
         batch size.
-
         Args:
             batch_emissions (chex.Array): Independent sequences.
             optmizer (optax.Optimizer): Optimizer.
@@ -200,10 +192,10 @@ class BaseHMM(SSM):
             num_epochs (int): Iterations made through entire dataset.
             shuffle (bool): Indicates whether to shuffle minibatches.
             key (chex.PRNGKey): RNG key to shuffle minibatches.
-
         Returns:
             losses: Output of loss_fn stored at each step.
         """
+
         def _loss_fn(params, minibatch_emissions):
             """Default objective function."""
             self.unconstrained_params = params
@@ -223,6 +215,7 @@ class BaseHMM(SSM):
         self.unconstrained_params = params
         return losses
 
+
 class StandardHMM(BaseHMM):
 
     def __init__(self,
@@ -233,7 +226,6 @@ class StandardHMM(BaseHMM):
         """
         Abstract base class for Hidden Markov Models.
         Child class specifies the emission distribution.
-
         Args:
             initial_probabilities[k]: prob(hidden(1)=k)
             transition_matrix[j,k]: prob(hidden(t) = k | hidden(t-1)j)
@@ -272,28 +264,22 @@ class StandardHMM(BaseHMM):
     @abstractmethod
     def emission_distribution(self, state):
         """Return a distribution over emissions given current state.
-
         Args:
             state (PyTree): current latent state.
-
         Returns:
             dist (tfd.Distribution): conditional distribution of current emission.
         """
         raise NotImplementedError
 
     def _m_step_initial_probs(self, batch_emissions, batch_posteriors):
-        post = tfd.Dirichlet(self._initial_probs_concentration.value +
-                             batch_posteriors.initial_probs.sum(axis=0))
+        post = tfd.Dirichlet(self._initial_probs_concentration.value + batch_posteriors.initial_probs.sum(axis=0))
         self._initial_probs.value = post.mode()
 
     def _m_step_transition_matrix(self, batch_emissions, batch_posteriors):
-        post = tfd.Dirichlet(self._transition_matrix_concentration.value +
-                             batch_posteriors.trans_probs.sum(axis=0))
+        post = tfd.Dirichlet(self._transition_matrix_concentration.value + batch_posteriors.trans_probs.sum(axis=0))
         self._transition_matrix.value = post.mode()
 
-    def _m_step_emissions(self, batch_emissions, batch_posteriors,
-                          optimizer=optax.adam(1e-2),
-                          num_mstep_iters=50):
+    def _m_step_emissions(self, batch_emissions, batch_posteriors, optimizer=optax.adam(1e-2), num_mstep_iters=50):
 
         def neg_expected_log_joint(params, minibatch):
             minibatch_emissions, minibatch_posteriors = minibatch
@@ -301,35 +287,27 @@ class StandardHMM(BaseHMM):
             self.unconstrained_params = params
 
             def _single_expected_log_like(emissions, posterior):
-                log_likelihoods = self._conditional_logliks(emissions)
+                log_likelihoods = self._compute_conditional_logliks(emissions)
                 expected_states = posterior.smoothed_probs
-                lp += jnp.sum(expected_states * log_likelihoods)
-                return lp
+                return jnp.sum(expected_states * log_likelihoods)
 
             log_prior = self.log_prior()
-            minibatch_ells = vmap(_single_expected_log_like)(
-                minibatch_emissions, minibatch_posteriors)
+            minibatch_ells = vmap(_single_expected_log_like)(minibatch_emissions, minibatch_posteriors)
             expected_log_joint = log_prior + minibatch_ells.sum() * scale
             return -expected_log_joint / batch_emissions.size
 
         # Minimize the negative expected log joint with SGD
         params, losses = run_sgd(neg_expected_log_joint,
-                                 self.unconstrained_params,
-                                 (batch_emissions, batch_posteriors),
+                                 self.unconstrained_params, (batch_emissions, batch_posteriors),
                                  optimizer=optimizer,
                                  num_epochs=num_mstep_iters)
         self.unconstrained_params = params
 
-    def m_step(self, batch_emissions, batch_posteriors,
-               optimizer=optax.adam(1e-2),
-               num_mstep_iters=50):
+    def m_step(self, batch_emissions, batch_posteriors, optimizer=optax.adam(1e-2), num_mstep_iters=50):
 
         self._m_step_initial_probs(batch_emissions, batch_posteriors)
         self._m_step_transition_matrix(batch_emissions, batch_posteriors)
-        self._m_step_emissions(batch_emissions, batch_posteriors,
-                               optimizer=optimizer,
-                               num_mstep_iters=num_mstep_iters)
-
+        self._m_step_emissions(batch_emissions, batch_posteriors, optimizer=optimizer, num_mstep_iters=num_mstep_iters)
 
 
 class ExponentialFamilyHMM(StandardHMM):
@@ -342,16 +320,15 @@ class ExponentialFamilyHMM(StandardHMM):
     def _zeros_like_suff_stats(self):
         raise NotImplementedError
 
-    def fit_stochastic_em(self,
-                          emissions_generator,
-                          total_emissions,
-                          schedule=None,
-                          num_epochs=50,
-        ):
-
+    def fit_stochastic_em(
+        self,
+        emissions_generator,
+        total_emissions,
+        schedule=None,
+        num_epochs=50,
+    ):
         """
         Fit this HMM by running Stochastic Expectation-Maximization.
-
         Assuming the original dataset consists of N independent sequences of
         length T, this algorithm performs EM on a random subset of B sequences 
         (not timesteps) at each step. Importantly, the subsets of B sequences
@@ -371,7 +348,6 @@ class ExponentialFamilyHMM(StandardHMM):
             schedule (optax schedule, Callable: int -> [0, 1]): Learning rate
                 schedule; defaults to exponential schedule.
             num_epochs (int): Num of iterations made through the entire dataset.
-
         Returns:
             expected_log_prob (chex.Array): Mean expected log prob of each epoch.
         
@@ -394,7 +370,7 @@ class ExponentialFamilyHMM(StandardHMM):
                 decay_rate=.95,
             )
 
-        learning_rates = schedule(jnp.arange(num_epochs*num_batches))
+        learning_rates = schedule(jnp.arange(num_epochs * num_batches))
         assert learning_rates[0] == 1.0, "Learning rate must start at 1."
         learning_rates = learning_rates.reshape(num_epochs, num_batches)
 
@@ -406,30 +382,22 @@ class ExponentialFamilyHMM(StandardHMM):
             # Compute the sufficient stats given a minibatch of emissions
             self.unconstrained_params = params
             minibatch_stats = self.e_step(minibatch_emissions)
-            
+
             # Scale the stats as if they came from the whole dataset
             scale = total_emissions / len(minibatch_emissions.reshape(-1, self.num_obs))
-            scaled_minibatch_stats = tree_map(
-                lambda x: jnp.sum(x, axis=0) * scale,
-                minibatch_stats
-            )
+            scaled_minibatch_stats = tree_map(lambda x: jnp.sum(x, axis=0) * scale, minibatch_stats)
             expected_lp = self.log_prior() + scaled_minibatch_stats.marginal_loglik
-            
+
             # Incorporate these these stats into the rolling averaged stats
-            rolling_stats = tree_map(
-                lambda s0, s1: (1-learn_rate) * s0 + learn_rate * s1,
-                rolling_stats,
-                scaled_minibatch_stats
-            )
+            rolling_stats = tree_map(lambda s0, s1: (1 - learn_rate) * s0 + learn_rate * s1, rolling_stats,
+                                     scaled_minibatch_stats)
 
             # Add a batch dimension and call M-step
-            batched_rolling_stats = tree_map(
-                lambda x: jnp.expand_dims(x, axis=0), rolling_stats
-            )
+            batched_rolling_stats = tree_map(lambda x: jnp.expand_dims(x, axis=0), rolling_stats)
             self.m_step(minibatch_emissions, batched_rolling_stats)
 
             return (self.unconstrained_params, rolling_stats), expected_lp
-        
+
         # Initialize and train
         expected_log_probs = []
         params = self.unconstrained_params
@@ -445,7 +413,7 @@ class ExponentialFamilyHMM(StandardHMM):
                 _expected_lps += expected_lp
 
             # Save epoch mean of expected log probs
-            expected_log_probs.append(_expected_lps/num_batches)
+            expected_log_probs.append(_expected_lps / num_batches)
 
         # Update self with fitted params
         self.unconstrained_params = params
