48265e37caefbd3d856eb7e67e7d1092a2efacb6
karalleyna <aleynakara98@gmail.com>
2022-08-08 21:12:40 +0000

    Add import statements for new GaussianHMM classes

--- ssm_jax/hmm/models/__init__.py
+++ ssm_jax/hmm/models/__init__.py
@@ -1,4 +1,7 @@
 from ssm_jax.hmm.models.bernoulli_hmm import BernoulliHMM
 from ssm_jax.hmm.models.categorical_hmm import CategoricalHMM
 from ssm_jax.hmm.models.gaussian_hmm import GaussianHMM
+from ssm_jax.hmm.models.mvn_diag_hmm import MultivariateNormalDiagHMM
+from ssm_jax.hmm.models.mvn_spherical_hmm import MultivariateNormalSphericalHMM
+from ssm_jax.hmm.models.mvn_tied_hmm import MultivariateNormalTiedHMM
 from ssm_jax.hmm.models.poisson_hmm import PoissonHMM
