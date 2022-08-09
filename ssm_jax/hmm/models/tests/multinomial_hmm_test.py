506b17ddc485217015fa9e1f35ea47e63b89feab
karalleyna <aleynakara98@gmail.com>
2022-08-09 13:06:06 +0000

    Add new test cases to multinomial_hmm_test.py

--- ssm_jax/hmm/models/tests/multinomial_hmm_test.py
+++ ssm_jax/hmm/models/tests/multinomial_hmm_test.py
@@ -2,7 +2,6 @@ import jax.numpy as jnp
 import jax.random as jr
 import pytest
 from ssm_jax.hmm.models.multinomial_hmm import MultinomialHMM
-from ssm_jax.hmm.models.tests.test_utils import normalized
 from ssm_jax.utils import monotonically_increasing
 
 
@@ -20,6 +19,18 @@ class TestMultinomialHMM:
         transition_matrix = jnp.array([[.8, .2], [.2, .8]])
         emission_probs = jnp.array([[[.5, .3, .1, .1]], [[.1, .1, .4, .4]]])
         return MultinomialHMM(initial_probabilities, transition_matrix, emission_probs, self.num_trials)
+
+    def test_random_initialization(self, key=jr.PRNGKey(0)):
+        hmm = MultinomialHMM.random_initialization(key, self.num_states, self.emission_dim, self.num_classes)
+
+        assert hmm.initial_probs.value.shape == (self.num_states,)
+        assert jnp.allclose(hmm.initial_probs.value.sum(), 1.)
+
+        assert hmm.transition_matrix.value.shape == (self.num_states, self.num_states)
+        assert jnp.allclose(hmm.transition_matrix.value.sum(axis=-1), 1.)
+
+        assert hmm.emission_probs.value.shape == (self.num_states, self.emission_dim, self.num_classes)
+        assert jnp.allclose(hmm.emission_probs.value.sum(axis=-1), 1.)
 
     def test_score_samples(self):
         X = jnp.array([
@@ -39,11 +50,12 @@ class TestMultinomialHMM:
 
         posteriors = hmm.filter(X)
         assert posteriors.filtered_probs.shape == (n_samples, self.num_states)
-        assert jnp.allclose(posteriors.filtered_probs.sum(axis=1), jnp.ones(n_samples))
+        assert jnp.allclose(posteriors.filtered_probs.sum(axis=1), 1.)
 
     def test_sample(self, key=jr.PRNGKey(0), num_timesteps=1000):
         hmm = self.new_hmm()
         state_sequence, emissions = hmm.sample(key, num_timesteps)
+
         assert emissions.ndim == 3
         assert len(emissions) == len(state_sequence) == num_timesteps
         assert len(jnp.unique(emissions)) == self.num_trials + 1
@@ -62,7 +74,7 @@ class TestMultinomialHMM:
 
         assert monotonically_increasing(lps, 1)
 
-    def test_fit_emission_probs(self, key=jr.PRNGKey(0), num_timesteps=100):
+    def test_smooth(self, key=jr.PRNGKey(0), num_timesteps=100):
 
         sample_key, init_key = jr.split(key, 2)
         true_hmm = self.new_hmm()
@@ -70,7 +82,8 @@ class TestMultinomialHMM:
 
         # Mess up the parameters and see if we can re-learn them.
         hmm = MultinomialHMM.random_initialization(init_key, self.num_states, self.emission_dim, self.num_classes)
-        lps = hmm.fit_em(emissions[None, ...])
-        """assert jnp.allclose(prev_initial_probs, hmm.initial_probs.value)
-        assert jnp.allclose(prev_transition_matrix, hmm.transition_matrix.value)"""
-        assert monotonically_increasing(lps, 1)
+
+        posteriors = hmm.smoother(emissions)
+
+        assert jnp.allclose(posteriors.filtered_probs.sum(axis=-1), 1.)
+        assert jnp.allclose(posteriors.smoothed_probs.sum(axis=-1), 1.)
