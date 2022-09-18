b98f8b76d9af8064fd3774a490524666a00aa2bd
Scott Linderman <scott.linderman@gmail.com>
2022-09-18 13:22:54 -0700

    updating poisson changepoint notebook

--- ssm_jax/hmm/models/__init__.py
+++ ssm_jax/hmm/models/__init__.py
@@ -1,3 +1,4 @@
+from ssm_jax.hmm.models.base import BaseHMM, StandardHMM
 from ssm_jax.hmm.models.bernoulli_hmm import BernoulliHMM
 from ssm_jax.hmm.models.categorical_glm_hmm import CategoricalRegressionHMM
 from ssm_jax.hmm.models.categorical_hmm import CategoricalHMM
