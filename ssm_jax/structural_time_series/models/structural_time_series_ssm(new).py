6f03b0995b771ec12bb9b72325d9c6ad7c8282d5
xinglong <xinglong.li@stat.ubc.ca>
2022-10-03 23:51:42 -0700

    wip---sts

--- ssm_jax/structural_time_series/models/structural_time_series_ssm(new).py
+++ ssm_jax/structural_time_series/models/structural_time_series_ssm(new).py
@@ -343,48 +343,6 @@ class GaussianSSM(_StructuralTimeSeriesSSM):
                            emission_bias=self.emission_bias,
                            emission_covariance=obs_cov)
 
-    def fit_hmc(self,
-                key,
-                sample_size,
-                emissions,
-                inputs=None,
-                warmup_steps=500,
-                num_integration_steps=30):
-
-        def logprob(trainable_unc_params):
-            params = from_unconstrained(trainable_unc_params, fixed_params, self.param_props)
-            log_det_jac = log_det_jac_constrain(trainable_unc_params, fixed_params, self.param_props)
-            log_pri = self.log_prior(params) + log_det_jac
-            batch_lls = self.marginal_log_prob(emissions, inputs, params)
-            lp = log_pri + batch_lls.sum()
-            return lp
-
-        # Initialize the HMC sampler using window_adaptations
-        hmc_initial_position, fixed_params = to_unconstrained(self.params, self.param_props)
-        warmup = blackjax.window_adaptation(blackjax.hmc,
-                                            logprob,
-                                            num_steps=warmup_steps,
-                                            num_integration_steps=num_integration_steps)
-        hmc_initial_state, hmc_kernel, _ = warmup.run(key, hmc_initial_position)
-
-        @jit
-        def _step(current_state, rng_key):
-            next_state, _ = hmc_kernel(rng_key, current_state)
-            unc_sample = next_state.position
-            return next_state, unc_sample
-
-        keys = iter(jr.split(key, sample_size))
-        param_samples = []
-        current_state = hmc_initial_state
-        for _ in trange(sample_size):
-            current_state, unc_sample = _step(current_state, next(keys))
-            sample = from_unconstrained(unc_sample, fixed_params, self.param_props)
-            param_samples.append(sample)
-
-        param_samples = tree_map(lambda x, *y: jnp.array([x] + [i for i in y]),
-                                 param_samples[0], *param_samples[1:])
-        return param_samples
-
     def forecast(self, key, observed_time_series, num_forecast_steps,
                  past_inputs=None, forecast_inputs=None):
         """Forecast the time series"""
@@ -489,48 +447,6 @@ class PoissonSSM(_StructuralTimeSeriesSSM):
                            emission_input_weights=emission_input_weights,
                            emission_bias=self.emission_bias,
                            emission_covariance=obs_cov)
-
-    def fit_hmc(self,
-                key,
-                sample_size,
-                emissions,
-                inputs=None,
-                warmup_steps=500,
-                num_integration_steps=30):
-
-        def logprob(trainable_unc_params):
-            params = from_unconstrained(trainable_unc_params, fixed_params, self.param_props)
-            log_det_jac = log_det_jac_constrain(trainable_unc_params, fixed_params, self.param_props)
-            log_pri = self.log_prior(params) + log_det_jac
-            batch_lls = self.marginal_log_prob(emissions, inputs, params)
-            lp = log_pri + batch_lls.sum()
-            return lp
-
-        # Initialize the HMC sampler using window_adaptations
-        hmc_initial_position, fixed_params = to_unconstrained(self.params, self.param_props)
-        warmup = blackjax.window_adaptation(blackjax.hmc,
-                                            logprob,
-                                            num_steps=warmup_steps,
-                                            num_integration_steps=num_integration_steps)
-        hmc_initial_state, hmc_kernel, _ = warmup.run(key, hmc_initial_position)
-
-        @jit
-        def _step(current_state, rng_key):
-            next_state, _ = hmc_kernel(rng_key, current_state)
-            unc_sample = next_state.position
-            return next_state, unc_sample
-
-        keys = iter(jr.split(key, sample_size))
-        param_samples = []
-        current_state = hmc_initial_state
-        for _ in trange(sample_size):
-            current_state, unc_sample = _step(current_state, next(keys))
-            sample = from_unconstrained(unc_sample, fixed_params, self.param_props)
-            param_samples.append(sample)
-
-        param_samples = tree_map(lambda x, *y: jnp.array([x] + [i for i in y]),
-                                 param_samples[0], *param_samples[1:])
-        return param_samples
 
     def forecast(self, key, observed_time_series, num_forecast_steps,
                  past_inputs=None, forecast_inputs=None):
