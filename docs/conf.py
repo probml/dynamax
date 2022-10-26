58ad80a5c1512d33838531ee00e04b27f8adcd7e
Scott Linderman <slinderman@users.noreply.github.com>
2022-10-26 10:03:01 -0700

    Update conf.py
    update ipynb extension. hope this fixes broken colab links!

--- docs/conf.py
+++ docs/conf.py
@@ -37,7 +37,7 @@ extensions = [
 source_suffix = {
     '.rst': 'restructuredtext',
     '.myst': 'myst-nb',
-    'ipynb': 'myst-nb'
+    '.ipynb': 'myst-nb'
 }
 
 # Add any paths that contain templates here, relative to this directory.
