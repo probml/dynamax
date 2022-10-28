77bde2ce5d1a73981f1e91b83560dd08078ce997
Scott Linderman <scott.linderman@gmail.com>
2022-10-28 11:28:58 -0700

    updating SphericalGaussianHMM

--- dynamax/hmm/models/gaussian_hmm.py
+++ dynamax/hmm/models/gaussian_hmm.py
@@ -9,6 +9,7 @@ from dynamax.distributions import NormalInverseWishart
 from dynamax.distributions import nig_posterior_update
 from dynamax.distributions import niw_posterior_update
 from dynamax.hmm.models.base import ExponentialFamilyHMM
+from dynamax.hmm.models.base import StandardHMM
 from dynamax.utils import PSDToRealBijector
 
 
@@ -194,14 +195,6 @@ class DiagonalGaussianHMM(ExponentialFamilyHMM):
     def emission_shape(self):
         return (self.emission_dim,)
 
-    # def _initialize_emissions(self, key):
-    #     key1, key2 = jr.split(key, 2)
-    #     emission_means = jr.normal(key1, (self.num_states, self.emission_dim))
-    #     emission_scale_diags = jr.exponential(key2, (self.num_states, self.emission_dim))
-    #     params = dict(means=emission_means, scale_diags=emission_scale_diags)
-    #     param_props = dict(means=ParameterProperties(), scale_diags=ParameterProperties(constrainer=tfb.Softplus()))
-    #     return  params, param_props
-
     def initialize(self, key=jr.PRNGKey(0),
                    method="prior",
                    initial_probs=None,
@@ -300,3 +293,109 @@ class DiagonalGaussianHMM(ExponentialFamilyHMM):
         params['emissions']['scale_diags'] = jnp.sqrt(vars)
         params['emissions']['means'] = means
         return params
+
+
+class SphericalGaussianHMM(StandardHMM):
+
+    def __init__(self,
+                 num_states,
+                 emission_dim,
+                 initial_probs_concentration=1.1,
+                 transition_matrix_concentration=1.1,
+                 emission_prior_mean=0.0,
+                 emission_prior_mean_covariance=1.0,
+                 emission_var_concentration=1.1,
+                 emission_var_rate=1.1):
+
+        super().__init__(num_states,
+                         initial_probs_concentration=initial_probs_concentration,
+                         transition_matrix_concentration=transition_matrix_concentration)
+
+        self.emission_dim = emission_dim
+        self.emission_prior_mean = emission_prior_mean * jnp.ones(emission_dim)
+        self.emission_prior_mean_cov = \
+            emission_prior_mean_covariance if jnp.ndim(emission_prior_mean_covariance) == 2 \
+                else emission_prior_mean_covariance * jnp.eye(emission_dim)
+        self.emission_var_concentration = emission_var_concentration
+        self.emission_var_rate = emission_var_rate
+
+    @property
+    def emission_shape(self):
+        return (self.emission_dim,)
+
+    def initialize(self, key=jr.PRNGKey(0),
+                   method="prior",
+                   initial_probs=None,
+                   transition_matrix=None,
+                   emission_means=None,
+                   emission_scales=None,
+                   emissions=None):
+        """Initialize the model parameters and their corresponding properties.
+
+        You can either specify parameters manually via the keyword arguments, or you can have
+        them set automatically. If any parameters are not specified, you must supply a PRNGKey.
+        Parameters will then be sampled from the prior (if `method==prior`).
+
+        Note: in the future we may support more initialization schemes, like K-Means.
+
+        Args:
+            key (PRNGKey, optional): random number generator for unspecified parameters. Must not be None if there are any unspecified parameters. Defaults to None.
+            method (str, optional): method for initializing unspecified parameters. Currently, only "prior" is allowed. Defaults to "prior".
+            initial_probs (array, optional): manually specified initial state probabilities. Defaults to None.
+            transition_matrix (array, optional): manually specified transition matrix. Defaults to None.
+            emission_means (array, optional): manually specified emission means. Defaults to None.
+            emission_scales (array, optional): manually specified emission scales (sqrt of diagonal of spherical covariance matrix). Defaults to None.
+            emissions (array, optional): emissions for initializing the parameters with kmeans. Defaults to None.
+
+        Returns:
+            params: a nested dictionary of arrays containing the model parameters.
+            props: a nested dictionary of ParameterProperties to specify parameter constraints and whether or not they should be trained.
+        """
+        # Base class initializes the initial probs and transition matrix
+        this_key, key = jr.split(key)
+        params, props = super().initialize(key=this_key, method=method,
+                                           initial_probs=initial_probs,
+                                           transition_matrix=transition_matrix)
+
+        if method.lower() == "kmeans":
+            assert emissions is not None, "Need emissions to initialize the model with K-Means!"
+            from sklearn.cluster import KMeans
+            km = KMeans(self.num_states).fit(emissions.reshape(-1, self.emission_dim))
+
+            _emission_means = jnp.array(km.cluster_centers_)
+            _emission_scales = jnp.ones((self.num_states,))
+
+        elif method.lower() == "prior":
+            key1, key2, key = jr.split(key, 3)
+            _emission_means = tfd.MultivariateNormalFullCovariance(
+                self.emission_prior_mean, self.emission_prior_mean_cov)\
+                    .sample(seed=key1, sample_shape=(self.num_states,))
+            _emission_vars = tfd.Gamma(
+                self.emission_var_concentration, self.emission_var_rate)\
+                    .sample(seed=key2, sample_shape=(self.num_states,))
+            _emission_scales = jnp.sqrt(_emission_vars)
+
+        else:
+            raise Exception("Invalid initialization method: {}".format(method))
+
+        # Only use the values above if the user hasn't specified their own
+        default = lambda x, x0: x if x is not None else x0
+        params['emissions'] = dict(means=default(emission_means, _emission_means),
+                                   scales=default(emission_scales, _emission_scales))
+        props['emissions'] = dict(means=ParameterProperties(),
+                                  scales=ParameterProperties(constrainer=tfb.Softplus()))
+        return params, props
+
+    def emission_distribution(self, params, state, covariates=None):
+        dim = self.emission_dim
+        return tfd.MultivariateNormalDiag(params['emissions']['means'][state],
+                                          params['emissions']['scales'][state] * jnp.ones((dim,)))
+
+    def log_prior(self, params):
+        lp = super().log_prior(params)
+        lp += tfd.MultivariateNormalFullCovariance(
+            self.emission_prior_mean, self.emission_prior_mean_cov)\
+                .log_prob(params['emissions']['means']).sum()
+        lp += tfd.Gamma(self.emission_var_concentration, self.emission_var_rate)\
+            .log_prob(params['emissions']['scales']**2).sum()
+        return lp
