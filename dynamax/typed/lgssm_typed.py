2ba257d319c1e30a85d2ad7684fa1b683c6c85a5
Kevin P Murphy <murphyk@gmail.com>
2022-10-28 22:43:21 -0700

    elaborate types

--- dynamax/typed/lgssm_typed.py
+++ dynamax/typed/lgssm_typed.py
@@ -1,20 +1,17 @@
 
 from dynamax.typed.types import *
+from dynamax.utils import PSDToRealBijector
 
+
+@dataclass
 class MyLGSSM(SSM):
-    def __init__(self,
-                 state_dim: int,
-                 emission_dim: int,
-                 covariate_dim: int = 0,
-                 has_dynamics_bias=True,
-                 has_emissions_bias=True):
-        self.state_dim = state_dim
-        self.emission_dim = emission_dim
-        self.covariate_dim = covariate_dim
-        self.has_dynamics_bias = has_dynamics_bias
-        self.has_emissions_bias = has_emissions_bias
+    state_dim: int
+    emission_dim: int
+    covariate_dim: int = 0
+    has_dynamics_bias: bool = True
+    has_emissions_bias: bool = True
 
-    def initialize_params(self, key: PRNGKey) -> Tuple[Params, ParamProps]:
+    def initialize_params_old(self, key: PRNGKey) -> Tuple[Params, ParamProps]:
         m = jnp.zeros(self.state_dim)
         S = jnp.eye(self.state_dim)
         F = 0.99 * jnp.eye(self.state_dim)
@@ -27,27 +24,61 @@ class MyLGSSM(SSM):
         R = 0.1 * jnp.eye(self.emission_dim)
         params = dict(
             initial=dict(mean=m, cov=S),
-            dynamics=dict(weights=F, bias=b, input_weights=B, cov=Q),
+            transitions=dict(weights=F, bias=b, input_weights=B, cov=Q),
             emissions=dict(weights=H, bias=d, input_weights=D, cov=R)
         )
         param_props = dict(
-            initial=dict(probs=ParameterProperties(trainable=False, constrainer=tfb.SoftmaxCentered())),
-            transitions=dict(transition_matrix=ParameterProperties(constrainer=tfb.SoftmaxCentered())),
-            emissions=dict(means=ParameterProperties(), scales=ParameterProperties(constrainer=tfb.Softplus(), trainable=False))
+            initial=dict(mean=ParameterProperties(),
+                         cov=ParameterProperties(constrainer=tfb.Invert(PSDToRealBijector))),
+            transitions=dict(weights=ParameterProperties(),
+                          bias=ParameterProperties(),
+                          input_weights=ParameterProperties(),
+                          cov=ParameterProperties(constrainer=tfb.Invert(PSDToRealBijector))),
+            emissions=dict(weights=ParameterProperties(),
+                          bias=ParameterProperties(),
+                          input_weights=ParameterProperties(),
+                          cov=ParameterProperties(constrainer=tfb.Invert(PSDToRealBijector)))
         )
         return (params, param_props)
 
+    def initialize_params(self, key: PRNGKey) -> Tuple[ParamsLGSSM, ParamPropsLGSSM]:
+        m = jnp.zeros(self.state_dim)
+        S = jnp.eye(self.state_dim)
+        F = 0.99 * jnp.eye(self.state_dim)
+        B = jnp.zeros((self.state_dim, self.covariate_dim))
+        b = jnp.zeros((self.state_dim,)) if self.has_dynamics_bias else None
+        Q = 0.1 * jnp.eye(self.state_dim)
+        H = jr.normal(key, (self.emission_dim, self.state_dim))
+        D = jnp.zeros((self.emission_dim, self.covariate_dim))
+        d = jnp.zeros((self.emission_dim,)) if self.has_emissions_bias else None
+        R = 0.1 * jnp.eye(self.emission_dim)
+        params = ParamsLGSSM(
+            initial_dist = GaussDist(mean=m, cov=S),
+            dynamics = CondGaussDistLatent(weights=F, bias=b, input_weights=B, cov=Q),
+            emissions = CondGaussDistObserved(weights=H, bias=d, input_weights=D, cov=R)
+        )
+        param_props = ParamPropsLGSSM(
+            initial_dist = ParamPropsGauss(mean=ParameterProperties(),
+                         cov=ParameterProperties(constrainer=tfb.Invert(PSDToRealBijector))),
+            dynamics = ParamPropsCondGauss(weights=ParameterProperties(),
+                          bias=ParameterProperties(),
+                          input_weights=ParameterProperties(),
+                          cov=ParameterProperties(constrainer=tfb.Invert(PSDToRealBijector))),
+            emissions = ParamPropsCondGauss(weights=ParameterProperties(),
+                          bias=ParameterProperties(),
+                          input_weights=ParameterProperties(),
+                          cov=ParameterProperties(constrainer=tfb.Invert(PSDToRealBijector)))
+        )
+        return (params, param_props)
 
-    def filter(self, params: Params, emissions: EmissionSeq, inputs: Optional[InputSeq]=None) -> GSSMPosterior:
-        print('filtering params ', params['initial']['mean'])
-        loglik = jnp.sum(emissions)
+    def filter(self, params: ParamsLGSSM, emissions: EmissionSeqVec, inputs: Optional[InputSeq]=None) -> GSSMPosterior:
+        #print('filtering params ', params['initial']['mean'])
+        print('filtering params ', params.initial_dist.mean)
+        loglik = jnp.sum(emissions) # random 
         ntime = emissions.shape[0]
         return GSSMPosterior(marginal_loglik = loglik, filtered_means = jnp.zeros((ntime, self.state_dim)))
 
-    def sample(self, params: Params, rng_key: PRNGKey, num_timesteps: int,  inputs: Optional[InputSeq]=None) -> Tuple[StateSeqVec, EmissionSeq]:
+    def sample(self, params: ParamsLGSSM, rng_key: PRNGKey, num_timesteps: int,  inputs: Optional[InputSeq]=None) -> Tuple[StateSeqVec, EmissionSeqVec]:
         states = jnp.zeros((num_timesteps, self.state_dim))
         emissions = jnp.zeros((num_timesteps, self.emission_dim))
         return (states, emissions)
-
-
-
--- dynamax/typed/types.py
+++ dynamax/typed/types.py
@@ -2,18 +2,23 @@
 from abc import ABC
 from abc import abstractmethod
 
+import jax
 #https://github.com/google/jaxtyping/blob/main/jaxtyping/__init__.py
-
 from jaxtyping import Array, Float, PyTree, Bool, Int
 from typeguard import typechecked
 
 import typing_extensions
 from typing_extensions import Protocol
-from typing import Any, Dict, NamedTuple, Optional, Tuple, Union,  TypeVar, Generic
+from typing import Any, Dict, NamedTuple, Optional, Tuple, Union,  TypeVar, Generic, Mapping
 
 #https://github.com/deepmind/chex/blob/master/chex/_src/pytypes.py
 import chex
-from chex import Array, ArrayTree, Scalar, Numeric, PRNGKey
+#from chex import Array, ArrayTree, Scalar, Numeric
+#from chex import PRNGKey
+
+#import jax._src.prng as prng
+#PRNGKey = prng.PRNGKeyArray
+PRNGKey = jax.random.PRNGKey
 
 from dataclasses import dataclass
 import jax
@@ -22,16 +27,12 @@ import jax.random as jr
 
 import tensorflow_probability.substrates.jax.bijectors as tfb
 
-EmissionSingle = Float[Array, "Nemit"]
-EmissionSeq = Float[Array, "Ntime Nemit"]
-EmissionBatch = Float[Array, "Nseq Ntime Nemit"]
+#LatentType = TypeVar('LatentType', bound=ArrayTree)
+#ObsType = TypeVar('ObsType', bound=ArrayTree)
 
-InputSingle = Float[Array, "Ninput"]
-InputSeq = Float[Array, "Ntime Ninput"]
-InputBatch = Float[Array, "Nseq Ntime Ninput"]
-
-LatentType = TypeVar('LatentType', bound=ArrayTree)
-ObsType = TypeVar('ObsType', bound=ArrayTree)
+InputSingle = Float[Array, "covariate_dim"]
+InputSeq = Float[Array, "ntime covariate_dim"]
+InputBatch = Float[Array, "nbatch ntime covariate_dim"]
 
 StateSingleDiscrete = Int
 StateSingleVec = Float[Array, "state_dim"]
@@ -45,27 +46,96 @@ StateBatchDiscrete = Int[Array, "nbatch ntime"]
 StateBatchVec = Float[Array, "nbatch ntime state_dim"]
 StateBatch = Union[StateBatchDiscrete, StateBatchVec]
 
+EmissionSingleDiscrete = Int
+EmissionSingleVec = Float[Array, "emission_dim"]
+EmissionSingle = Union[EmissionSingleDiscrete, EmissionSingleVec]
 
-#StateSingle = Float[Array, "Nstate"]
-#StateSeq = Float[Array, "Ntime Nstate"]
-#StateBatch = Float[Array, "Nseq Ntime Nstate"]
+EmissionSeqDiscrete = Int[Array, "ntime"]
+EmissionSeqVec = Float[Array, "ntime emission_dim"]
+EmissionSeq = Union[EmissionSeqDiscrete, EmissionSeqVec]
 
+EmissionBatchDiscrete = Int[Array, "nbatch ntime"]
+EmissionBatchVec = Float[Array, "nbatch ntime emission_dim"]
+EmissionBatch = Union[EmissionBatchDiscrete, EmissionBatchVec]
 
-@chex.dataclass
+@dataclass
 class ParameterProperties:
     trainable: bool = True
     constrainer: tfb.Bijector = tfb.Identity()
 
-Params = PyTree
-ParamProps = PyTree[ParameterProperties]
+
+#Params = PyTree
+#ParamProps = PyTree[ParameterProperties]
+
+@dataclass
+class ParamsHMM:
+    initial_dist: Float[Array, "nstates"]
+    transition_matrix:  Float[Array, "nstates nstates"]
+    emissions: PyTree # many kinds of p(y|z) distributions
+    likelihoods: Optional[Float[Array, "ntime nstates"]] = None # local evidence vector, lik(t,k) = p(y(t) | Z(t)=k)
+
+@dataclass
+class GaussDist:
+    mean: Float[Array, "state_dim"]
+    cov: Float[Array, "state_dim state_dim"]
+
+@dataclass
+class CondGaussDistLatent:
+    cov: Float[Array, "state_dim state_dim"]
+    weights: Float[Array, "state_dim state_dim"]
+    bias: Optional[Float[Array, "state_dim"]] = None
+    input_weights:  Optional[Float[Array, "covariates_dim state_dim"]] = None
 
 
+@dataclass
+class CondGaussDistObserved:
+    cov: Float[Array, "emission_dim emission_dim"]
+    weights: Float[Array, "emission_dim state_dim"]
+    bias: Optional[Float[Array, "emission_dim"]] = None
+    input_weights:  Optional[Float[Array, "covariates_dim emission_dim"]] = None
+
+@dataclass
+class ParamsLGSSM:
+    initial_dist: GaussDist
+    dynamics: CondGaussDistLatent
+    emissions: CondGaussDistObserved
+
+Params = Union[ParamsHMM, ParamsLGSSM]
+
+@dataclass
+class ParamPropsHMM:
+    initial_dist: ParameterProperties
+    transition_matrix: ParameterProperties
+    emissions: Mapping[str, ParameterProperties]
+
+@dataclass
+class ParamPropsGauss:
+    cov:  ParameterProperties
+    mean:  ParameterProperties
+
+@dataclass
+class ParamPropsCondGauss:
+    cov:  ParameterProperties
+    weights:  ParameterProperties
+    bias: Optional[ParameterProperties] = None
+    input_weights: Optional[ParameterProperties] = None
+
+@dataclass
+class ParamPropsLGSSM:
+    initial_dist: ParamPropsGauss
+    dynamics: ParamPropsCondGauss
+    emissions: ParamPropsCondGauss
+
+ParamProps = Union[ParamPropsHMM, ParamPropsLGSSM]
+
+
+@dataclass
 class SSMPosterior(ABC):
     """Store the output of filtering or smoothing.
     The fields depend on the type of SSM, so this class will be subclassed.
     """
 
-@chex.dataclass
+@dataclass
 class GSSMPosterior(SSMPosterior):
     """Simple wrapper for properties of an Gaussian SSM posterior distribution.
 
@@ -104,14 +174,10 @@ class HMMPosterior(SSMPosterior):
 
 class SSM(ABC):
     @abstractmethod
-    def initialize_params(self, rng_key: PRNGKey) -> Tuple[Params, ParamProps]:
-        """Create initial parameters at random."""
-        raise NotImplementedError
+    def initialize_params(self, rng_key: PRNGKey) -> Tuple[Params, ParamProps]: ...
 
     @abstractmethod
-    def filter(self, params: Params, emissions: EmissionSeq, inputs: Optional[InputSeq]=None) -> SSMPosterior:
-        """Compute filtered belief states."""
-        raise NotImplementedError
+    def filter(self, params: Params, emissions: EmissionSeq, inputs: Optional[InputSeq]=None) -> SSMPosterior: ...
 
     @abstractmethod
     def sample(self, params: Params, rng_key: PRNGKey, num_timesteps: Int,  inputs: Optional[InputSeq]=None) -> Tuple[StateSeq, EmissionSeq]:
