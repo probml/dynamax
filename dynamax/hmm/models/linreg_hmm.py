625bb00cc40236d1861f276fa898262a79662e30
Scott Linderman <scott.linderman@gmail.com>
2022-11-01 21:25:35 -0400

    update linear regression HMM

--- dynamax/hmm/models/linreg_hmm.py
+++ dynamax/hmm/models/linreg_hmm.py
@@ -1,24 +1,23 @@
 import jax.numpy as jnp
 import jax.random as jr
 from jax import vmap
-from dynamax.hmm.models.abstractions import ExponentialFamilyHMM
+from dynamax.hmm.models.abstractions import HMM, HMMEmissions
+from dynamax.hmm.models.initial import StandardHMMInitialState
+from dynamax.hmm.models.transitions import StandardHMMTransitions
 from dynamax.parameters import ParameterProperties
-from dynamax.utils import PSDToRealBijector
+from dynamax.utils import PSDToRealBijector, pytree_sum
 from tensorflow_probability.substrates import jax as tfp
 
 tfd = tfp.distributions
 tfb = tfp.bijectors
 
 
-class LinearRegressionHMM(ExponentialFamilyHMM):
+class LinearRegressionHMMEmissions(HMMEmissions):
 
     def __init__(self,
                  num_states,
                  covariate_dim,
-                 emission_dim,
-                 initial_probs_concentration=1.1,
-                 transition_matrix_concentration=1.1
-                 ):
+                 emission_dim):
         """_summary_
 
         Args:
@@ -28,9 +27,7 @@ class LinearRegressionHMM(ExponentialFamilyHMM):
             emission_biases (_type_): _description_
             emission_covariance_matrices (_type_): _description_
         """
-        super().__init__(num_states,
-                         initial_probs_concentration=initial_probs_concentration,
-                         transition_matrix_concentration=transition_matrix_concentration)
+        self.num_states = num_states
         self.covariate_dim = covariate_dim
         self.emission_dim = emission_dim
 
@@ -41,6 +38,103 @@ class LinearRegressionHMM(ExponentialFamilyHMM):
     @property
     def covariates_shape(self):
         return (self.covariate_dim,)
+
+    def initialize(self,
+                   key=jr.PRNGKey(0),
+                   method="prior",
+                   emission_weights=None,
+                   emission_biases=None,
+                   emission_covariances=None,
+                   emissions=None):
+        if method.lower() == "kmeans":
+            assert emissions is not None, "Need emissions to initialize the model with K-Means!"
+            from sklearn.cluster import KMeans
+            km = KMeans(self.num_states).fit(emissions.reshape(-1, self.emission_dim))
+            _emission_weights = jnp.zeros((self.num_states, self.emission_dim, self.covariate_dim))
+            _emission_biases = jnp.array(km.cluster_centers_)
+            _emission_covs = jnp.tile(jnp.eye(self.emission_dim)[None, :, :], (self.num_states, 1, 1))
+
+        elif method.lower() == "prior":
+            # TODO: Use an MNIW prior
+            key1, key2, key = jr.split(key, 3)
+            _emission_weights = 0.01 * jr.normal(key1, (self.num_states, self.emission_dim, self.covariate_dim))
+            _emission_biases = jr.normal(key2, (self.num_states, self.emission_dim))
+            _emission_covs = jnp.tile(jnp.eye(self.emission_dim), (self.num_states, 1, 1))
+        else:
+            raise Exception("Invalid initialization method: {}".format(method))
+
+        # Only use the values above if the user hasn't specified their own
+        default = lambda x, x0: x if x is not None else x0
+        params = dict(weights=default(emission_weights, _emission_weights),
+                      biases=default(emission_biases, _emission_biases),
+                      covs=default(emission_covariances, _emission_covs))
+        props = dict(weights=ParameterProperties(),
+                     biases=ParameterProperties(),
+                     covs=ParameterProperties(constrainer=tfb.Invert(PSDToRealBijector)))
+        return params, props
+
+    def distribution(self, params, state, covariates):
+        prediction = params["weights"][state] @ covariates
+        prediction +=  params["biases"][state]
+        return tfd.MultivariateNormalFullCovariance(prediction, params["covs"][state])
+
+    def log_prior(self, params):
+        return 0.0
+
+    # Expectation-maximization (EM) code
+    def collect_suff_stats(self, posterior, emissions, covariates=None):
+        expected_states = posterior.smoothed_probs
+        sum_w = jnp.einsum("tk->k", expected_states)
+        sum_x = jnp.einsum("tk,ti->ki", expected_states, covariates)
+        sum_y = jnp.einsum("tk,ti->ki", expected_states, emissions)
+        sum_xxT = jnp.einsum("tk,ti,tj->kij", expected_states, covariates, covariates)
+        sum_xyT = jnp.einsum("tk,ti,tj->kij", expected_states, covariates, emissions)
+        sum_yyT = jnp.einsum("tk,ti,tj->kij", expected_states, emissions, emissions)
+        return dict(sum_w=sum_w, sum_x=sum_x, sum_y=sum_y, sum_xxT=sum_xxT, sum_xyT=sum_xyT, sum_yyT=sum_yyT)
+
+    def m_step(self, params, props, batch_stats):
+        def _single_m_step(stats):
+            sum_w = stats['sum_w']
+            sum_x = stats['sum_x']
+            sum_y = stats['sum_y']
+            sum_xxT = stats['sum_xxT']
+            sum_xyT = stats['sum_xyT']
+            sum_yyT = stats['sum_yyT']
+
+            # Make block matrices for stacking features (x) and bias (1)
+            sum_x1x1T = jnp.block(
+                [[sum_xxT,                   jnp.expand_dims(sum_x, 1)],
+                 [jnp.expand_dims(sum_x, 0), jnp.expand_dims(sum_w, (0, 1))]]
+            )
+            sum_x1yT = jnp.vstack([sum_xyT, sum_y])
+
+            # Solve for the optimal A, b, and Sigma
+            Ab = jnp.linalg.solve(sum_x1x1T, sum_x1yT).T
+            Sigma = 1 / sum_w * (sum_yyT - Ab @ sum_x1yT)
+            Sigma = 0.5 * (Sigma + Sigma.T)                 # for numerical stability
+            return Ab[:, :-1], Ab[:, -1], Sigma
+
+        emission_stats = pytree_sum(batch_stats, axis=0)
+        As, bs, Sigmas = vmap(_single_m_step)(emission_stats)
+        params["weights"] = As
+        params["biases"] = bs
+        params["covs"] = Sigmas
+        return params
+
+
+class LinearRegressionHMM(HMM):
+    def __init__(self,
+                 num_states,
+                 covariate_dim,
+                 emission_dim,
+                 initial_probs_concentration=1.1,
+                 transition_matrix_concentration=1.1):
+        self.emission_dim = emission_dim
+        self.covariates_shape = covariate_dim
+        initial_component = StandardHMMInitialState(num_states, initial_probs_concentration=initial_probs_concentration)
+        transition_component = StandardHMMTransitions(num_states, transition_matrix_concentration=transition_matrix_concentration)
+        emission_component = LinearRegressionHMMEmissions(num_states, covariate_dim, emission_dim)
+        super().__init__(num_states, initial_component, transition_component, emission_component)
 
     def initialize(self,
                    key=jr.PRNGKey(0),
@@ -73,95 +167,13 @@ class LinearRegressionHMM(ExponentialFamilyHMM):
             params: a nested dictionary of arrays containing the model parameters.
             props: a nested dictionary of ParameterProperties to specify parameter constraints and whether or not they should be trained.
         """
-        # Base class initializes the initial probs and transition matrix
-        this_key, key = jr.split(key)
-        params, props = super().initialize(key=this_key, method=method,
-                                           initial_probs=initial_probs,
-                                           transition_matrix=transition_matrix)
-
-        if method.lower() == "kmeans":
-            assert emissions is not None, "Need emissions to initialize the model with K-Means!"
-            from sklearn.cluster import KMeans
-            km = KMeans(self.num_states).fit(emissions.reshape(-1, self.emission_dim))
-
-            _emission_weights = jnp.zeros((self.num_states, self.emission_dim, self.covariate_dim))
-            _emission_biases = jnp.array(km.cluster_centers_)
-            _emission_covs = jnp.tile(jnp.eye(self.emission_dim)[None, :, :], (self.num_states, 1, 1))
-
-        elif method.lower() == "prior":
-            # TODO: Use an MNIW prior
-            key1, key2, key = jr.split(key, 3)
-            _emission_weights = 0.01 * jr.normal(key1, (self.num_states, self.emission_dim, self.covariate_dim))
-            _emission_biases = jr.normal(key2, (self.num_states, self.emission_dim))
-            _emission_covs = jnp.tile(jnp.eye(self.emission_dim), (self.num_states, 1, 1))
+        if key is not None:
+            key1, key2, key3 = jr.split(key , 3)
         else:
-            raise Exception("Invalid initialization method: {}".format(method))
+            key1 = key2 = key3 = None
 
-        # Only use the values above if the user hasn't specified their own
-        default = lambda x, x0: x if x is not None else x0
-        params['emissions'] = dict(weights=default(emission_weights, _emission_weights),
-                                   biases=default(emission_biases, _emission_biases),
-                                   covs=default(emission_covariances, _emission_covs))
-        props['emissions'] = dict(weights=ParameterProperties(),
-                                  biases=ParameterProperties(),
-                                  covs=ParameterProperties(constrainer=tfb.Invert(PSDToRealBijector)))
+        params, props = dict(), dict()
+        params["initial"], props["initial"] = self.initial_component.initialize(key1, method=method, initial_probs=initial_probs)
+        params["transitions"], props["transitions"] = self.transition_component.initialize(key2, method=method, transition_matrix=transition_matrix)
+        params["emissions"], props["emissions"] = self.emission_component.initialize(key3, method=method, emission_weights=emission_weights, emission_biases=emission_biases, emission_covariances=emission_covariances, emissions=emissions)
         return params, props
-
-    def emission_distribution(self, params, state, covariates):
-        prediction = params["emissions"]["weights"][state] @ covariates
-        prediction +=  params["emissions"]["biases"][state]
-        return tfd.MultivariateNormalFullCovariance(prediction, params["emissions"]["covs"][state])
-
-    def log_prior(self, params):
-        lp = tfd.Dirichlet(self.initial_probs_concentration).log_prob(params['initial']['probs'])
-        lp += tfd.Dirichlet(self.transition_matrix_concentration).log_prob(
-            params['transitions']['transition_matrix']).sum()
-        # TODO: Add MatrixNormalInverseWishart prior
-        return lp
-
-    # Expectation-maximization (EM) code
-    def _zeros_like_suff_stats(self):
-        return dict(sum_w=jnp.zeros((self.num_states)),
-                    sum_x=jnp.zeros((self.num_states, self.covariate_dim)),
-                    sum_y=jnp.zeros((self.num_states, self.emission_dim)),
-                    sum_xxT=jnp.zeros((self.num_states, self.covariate_dim, self.covariate_dim)),
-                    sum_xyT=jnp.zeros((self.num_states, self.covariate_dim, self.emission_dim)),
-                    sum_yyT=jnp.zeros((self.num_states, self.emission_dim, self.emission_dim)))
-
-    def _compute_expected_suff_stats(self, params, emissions, expected_states, covariates):
-        sum_w = jnp.einsum("tk->k", expected_states)
-        sum_x = jnp.einsum("tk,ti->ki", expected_states, covariates)
-        sum_y = jnp.einsum("tk,ti->ki", expected_states, emissions)
-        sum_xxT = jnp.einsum("tk,ti,tj->kij", expected_states, covariates, covariates)
-        sum_xyT = jnp.einsum("tk,ti,tj->kij", expected_states, covariates, emissions)
-        sum_yyT = jnp.einsum("tk,ti,tj->kij", expected_states, emissions, emissions)
-        return dict(sum_w=sum_w, sum_x=sum_x, sum_y=sum_y, sum_xxT=sum_xxT, sum_xyT=sum_xyT, sum_yyT=sum_yyT)
-
-    def _m_step_emissions(self, params, param_props, emission_stats):
-        def _single_m_step(stats):
-            # Unpack stats
-            sum_w = stats['sum_w']
-            sum_x = stats['sum_x']
-            sum_y = stats['sum_y']
-            sum_xxT = stats['sum_xxT']
-            sum_xyT = stats['sum_xyT']
-            sum_yyT = stats['sum_yyT']
-
-            # Make block matrices for stacking features (x) and bias (1)
-            sum_x1x1T = jnp.block(
-                [[sum_xxT,                   jnp.expand_dims(sum_x, 1)],
-                 [jnp.expand_dims(sum_x, 0), jnp.expand_dims(sum_w, (0, 1))]]
-            )
-            sum_x1yT = jnp.vstack([sum_xyT, sum_y])
-
-            # Solve for the optimal A, b, and Sigma
-            Ab = jnp.linalg.solve(sum_x1x1T, sum_x1yT).T
-            Sigma = 1 / sum_w * (sum_yyT - Ab @ sum_x1yT)
-            Sigma = 0.5 * (Sigma + Sigma.T)                 # for numerical stability
-            return Ab[:, :-1], Ab[:, -1], Sigma
-
-        As, bs, Sigmas = vmap(_single_m_step)(emission_stats)
-        params["emissions"]["weights"] = As
-        params["emissions"]["biases"] = bs
-        params["emissions"]["covs"] = Sigmas
-        return params
