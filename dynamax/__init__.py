87b1be816f8d0143075a4b9803d365ebc3e14f52
Kevin P Murphy <murphyk@gmail.com>
2022-11-09 21:54:13 -0800

    update comments in parameters.py

--- dynamax/__init__.py
+++ dynamax/__init__.py
@@ -1,1 +1,5 @@
 import dynamax.warnings
+
+from jaxtyping import install_import_hook
+with install_import_hook("dynamax", ("beartype", "beartype")):
+    from .linear_gaussian_ssm.inference import ParamsLGSSMInitial
---
+++ dynamax/types_test.py
@@ -0,0 +1,8 @@
+import dynamax
+from dynamax import ParamsLGSSMInitial
+
+def f(x):
+    print(x)
+
+x = 10
+f(x)
