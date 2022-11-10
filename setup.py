889204da640ed2ed36235746f4e5f218bfc49ad0
Scott Linderman <scott.linderman@gmail.com>
2022-11-09 21:21:16 -0800

    removing commented code

--- setup.py
+++ setup.py
@@ -16,20 +16,3 @@ if __name__ == '__main__':
           cmdclass=versioneer.get_cmdclass(),
           setup_requires=SETUP_REQUIRES,
           )
-
-# #!/usr/bin/env python
-# from distutils.core import setup
-# import setuptools
-
-# with open("requirements.txt") as f:
-#     requirements = list(map(lambda x: x.strip(), f.read().strip().splitlines()))
-
-# setup(
-#     name="dynamax",
-#     version="0.1",
-#     description="JAX code for state space modeling and inference",
-#     url="https://github.com/probml/dynamax",
-#     install_requires=requirements,
-#     packages=setuptools.find_packages(),
-# )
-
