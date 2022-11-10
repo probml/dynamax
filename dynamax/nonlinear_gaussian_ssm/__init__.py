50f798f9e422ba3aa0696541bc97b2e015f653a3
Scott Linderman <scott.linderman@gmail.com>
2022-11-10 12:07:07 -0800

    exposing key functions for ggssm and nlgssm in __init__

---
+++ dynamax/nonlinear_gaussian_ssm/__init__.py
@@ -0,0 +1,8 @@
+from dynamax.nonlinear_gaussian_ssm.models import ParamsNLGSSM, NonlinearGaussianSSM
+from dynamax.nonlinear_gaussian_ssm.inference_ekf import extended_kalman_filter
+from dynamax.nonlinear_gaussian_ssm.inference_ekf import iterated_extended_kalman_filter
+from dynamax.nonlinear_gaussian_ssm.inference_ekf import extended_kalman_smoother
+from dynamax.nonlinear_gaussian_ssm.inference_ekf import iterated_extended_kalman_smoother
+from dynamax.nonlinear_gaussian_ssm.inference_ukf import unscented_kalman_filter
+from dynamax.nonlinear_gaussian_ssm.inference_ukf import unscented_kalman_smoother
+from dynamax.nonlinear_gaussian_ssm.inference_ukf import UKFHyperParams
