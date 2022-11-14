4be6230bdb624c0691f0fc96eb85ededf57bd29f
Scott Linderman <scott.linderman@gmail.com>
2022-11-14 08:33:22 -0800

    Making default matmul precision of float32 to address tpu bugs

--- dynamax/__init__.py
+++ dynamax/__init__.py
@@ -1,4 +1,9 @@
 from . import _version
 __version__ = _version.get_versions()['version']
 
+# Catch expected warnings from TFP
 import dynamax.warnings
+
+# Default to float32 matrix multiplication on TPUs and GPUs
+import jax
+jax.config.update('jax_default_matmul_precision', 'float32')
