39c504834314ec8166e3d511a63cc0a87ed3a290
Kevin P Murphy <murphyk@gmail.com>
2022-12-12 18:07:59 -0800

    remove unused progress_bar_scan from dekf/utils.py and tqdm dependency

--- dynamax/generalized_gaussian_ssm/dekf/utils.py
+++ dynamax/generalized_gaussian_ssm/dekf/utils.py
@@ -8,7 +8,7 @@ from jax import lax
 import flax.linen as nn
 from jax.flatten_util import ravel_pytree
 from jax.experimental import host_callback
-import tqdm
+
 
 ### MLP
 
@@ -88,82 +88,3 @@ def loss_optax(params, x, y, loss_fn, apply_fn):
 
 # Define SGD optimizer
 sgd_optimizer = optax.sgd(learning_rate=1e-2)
-
-
-
-### TQDM
-def progress_bar_scan(num_samples, message=None):
-    "Progress bar for a JAX scan"
-    if message is None:
-            message = f"Running for {num_samples:,} iterations"
-    tqdm_bars = {}
-
-    if num_samples > 20:
-        print_rate = int(num_samples / 20)
-    else:
-        print_rate = 1 # if you run the sampler for less than 20 iterations
-    remainder = num_samples % print_rate
-
-    def _define_tqdm(arg, transform):
-        tqdm_bars[0] = tqdm(range(num_samples))
-        tqdm_bars[0].set_description(message, refresh=False)
-
-    def _update_tqdm(arg, transform):
-        tqdm_bars[0].update(arg)
-
-    def _update_progress_bar(iter_num):
-        "Updates tqdm progress bar of a JAX scan or loop"
-        _ = lax.cond(
-            iter_num == 0,
-            lambda _: host_callback.id_tap(_define_tqdm, None, result=iter_num),
-            lambda _: iter_num,
-            operand=None,
-        )
-
-        _ = lax.cond(
-            # update tqdm every multiple of `print_rate` except at the end
-            (iter_num % print_rate == 0) & (iter_num != num_samples-remainder),
-            lambda _: host_callback.id_tap(_update_tqdm, print_rate, result=iter_num),
-            lambda _: iter_num,
-            operand=None,
-        )
-
-        _ = lax.cond(
-            # update tqdm by `remainder`
-            iter_num == num_samples-remainder,
-            lambda _: host_callback.id_tap(_update_tqdm, remainder, result=iter_num),
-            lambda _: iter_num,
-            operand=None,
-        )
-
-    def _close_tqdm(arg, transform):
-        tqdm_bars[0].close()
-
-    def close_tqdm(result, iter_num):
-        return lax.cond(
-            iter_num == num_samples-1,
-            lambda _: host_callback.id_tap(_close_tqdm, None, result=result),
-            lambda _: result,
-            operand=None,
-        )
-
-
-    def _progress_bar_scan(func):
-        """Decorator that adds a progress bar to `body_fun` used in `lax.scan`.
-        Note that `body_fun` must either be looping over `np.arange(num_samples)`,
-        or be looping over a tuple who's first element is `np.arange(num_samples)`
-        This means that `iter_num` is the current iteration number
-        """
-
-        def wrapper_progress_bar(carry, x):
-            if type(x) is tuple:
-                iter_num, *_ = x
-            else:
-                iter_num = x   
-            _update_progress_bar(iter_num)
-            result = func(carry, x)
-            return close_tqdm(result, iter_num)
-
-        return wrapper_progress_bar
-
-    return _progress_bar_scan
