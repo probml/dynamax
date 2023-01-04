47672b9516e300b32a679d8c42be1d7b7dd4330c
Gerardo Duran-Martin <g.duran@me.com>
2023-01-04 11:28:37 +0000

    feat: gauss_approx.py
    Define sample_lrvga; compute_lrvga_cov

--- dynamax/rebayes/gauss_approx.py
+++ dynamax/rebayes/gauss_approx.py
@@ -1,7 +1,11 @@
 """
 Implementation of the Recursive Variational Gaussian Approximation
 (R-VGA) and the Limited-memory Recursive Variational Gaussian Approximation
-(LR-VGA) algorithms for sequential estimation.
+(LR-VGA) [1] algorithms for sequential estimation.
+
+[1] Lambert, M., Bonnabel, S., & Bach, F. (2021, December).
+The limited-memory recursive variational Gaussian approximation (L-RVGA).
+Retrieved from https://hal.inria.fr/hal-03501920
 """
 
 import jax
@@ -88,6 +92,49 @@ def fa_approx_step(
     return new_state
 
 
+def sample_lrvga(key, state, n_samples=10):
+    """
+    Sample from a low-rank variational Gaussian approximation.
+    This implementation avoids the explicit construction of the
+    (D x D) covariance matrix.
+
+    We take s ~ N(0, W W^T + Psi I)
+
+    Implementation based on §4.2.2 of the L-RVGA paper.
+    """
+    key_x, key_eps = jax.random.split(key)
+    dim_full, dim_latent = state.W.shape
+    Psi_inv = 1 / state.Psi
+
+    eps_sample = jax.random.normal(key_eps, (n_samples, dim_latent,))
+    x_sample = jax.random.normal(key_x, (n_samples, dim_full)) * jnp.sqrt(Psi_inv)
+
+    I_full = jnp.eye(dim_full)
+    I_latent = jnp.eye(dim_latent)
+    # M = I + W^T Psi^{-1} W
+    M = I_latent + jnp.einsum("ji,j,jk->ik", state.W, Psi_inv, state.W)
+    # L = Psi^{-1} W^T M^{-1}
+    L_tr = jnp.linalg.solve(M.T, jnp.einsum("i,ij->ji", Psi_inv, state.W))
+
+    # samples = (I - LW^T)x + Le
+    term1 = I_full - jnp.einsum("ji,kj->ik", L_tr, state.W)
+    x_transform = jnp.einsum("ij,nj->ni", term1, x_sample)
+    eps_transform = jnp.einsum("ji,nj->ni", L_tr, eps_sample)
+    samples = x_transform + eps_transform
+    return samples
+
+
+def compute_lrvga_cov(state, x, y, link_fn, cov_fn):
+    """
+    Compute the approximated expected covariance matrix
+    of the posterior distribution.
+
+    Implementation based on §4.2.4 of the L-RVGA paper.
+    """
+    raise NotImplementedError("TODO")
+    # jax.vjp()
+
+
 @jax.jit
 def mu_update(
     state: LRVGAState,
@@ -100,7 +147,10 @@ def mu_update(
     _, dim_latent = W.shape
     I = jnp.eye(dim_latent)
     
-    err = y - jnp.einsum("i,i->", mu, x)
+    # mu_samples = sample_lrvga(key, state, n_samples=10) + mu
+    # yhat = state.apply_fn(mu_samples, x).mean(axis=0)
+    yhat = jnp.einsum("i,i->", mu, x)
+    err = y - yhat
 
     M = I + jnp.einsum("ij,i,ik->jk", W, Psi_inv, W)
     b_matrix = jnp.einsum("ij,i,i->j", W, Psi_inv, x)
@@ -113,6 +163,8 @@ def mu_update(
 def _step_lrvga(state, obs, alpha, beta, n_inner):
     x, y = obs
 
+    # Xt = compute_lrvga_cov(state, x, y)
+    # Then, replace x with Xt
     def fa_partial(_, new_state):
         new_state = fa_approx_step(x, new_state, state, alpha, beta)
         return new_state
