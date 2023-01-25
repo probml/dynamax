4ac3723374143f8bafcd0dd8b5c32f95bae48992
Peter G. Chang <peter0414@gmail.com>
2023-01-25 11:24:21 -0600

    Implement recursive looped version of orfit

--- dynamax/rebayes/orfit.py
+++ dynamax/rebayes/orfit.py
@@ -14,6 +14,7 @@ from jax import vmap
 from jax.lax import scan
 from jaxtyping import Float, Array
 from typing import Callable, NamedTuple
+import chex
 
 from dynamax.nonlinear_gaussian_ssm.models import FnStateAndInputToEmission
 
@@ -47,6 +48,44 @@ class PosteriorORFitFiltered(NamedTuple):
     filtered_bases: Float[Array, "ntime state_dim memory_size"]
 
 
+def _orfit_condition_on(m, U, Sigma, loss_fn, apply_fn, x, y):
+    """Condition on the emission using orfit
+
+    Args:
+        m (D_hid): Prior mean.
+        U (D_hid, D_mem): Prior basis.
+        Sigma (D_mem): Prior singular values.
+        loss_fn (Callable): Loss function.
+        apply_fn (Callable): Apply function.
+        x (D_in): Control input.
+        y (D_obs): Emission.
+
+    Returns:
+        m_cond (D_hid): Posterior mean.
+        U_cond (D_hid, D_mem): Posterior basis.
+        Sigma_cond (D_mem): Posterior singular values.
+    """    
+    l_fn = lambda w: loss_fn(w, x, y)
+    f_fn = lambda w: apply_fn(w, x)
+
+    # Compute Jacobians and project out the orthogonal components
+    g = jacrev(l_fn)(m).squeeze()
+    v = jacrev(f_fn)(m).squeeze()
+    g_tilde = g - _project_to_columns(U, g)
+    v_prime = v - _project_to_columns(U, v)
+
+    # Update the U matrix
+    u = _normalize(v_prime)
+    U_cond = jnp.where(Sigma.min() > u @ v_prime, U, U.at[:, Sigma.argmin()].set(u))
+    Sigma_cond = jnp.where(Sigma.min() > u @ v_prime, Sigma, Sigma.at[Sigma.argmin()].set(u.T @ v_prime))
+
+    # Update the parameters
+    eta = _stable_division((f_fn(m) - y), (v.T @ g_tilde))
+    m_cond = m - eta * g_tilde
+
+    return m_cond, U_cond, Sigma_cond
+
+
 def orthogonal_recursive_fitting(
     model_params: ORFitParams,
     emissions: Float[Array, "ntime emission_dim"],
@@ -71,25 +110,54 @@ def orthogonal_recursive_fitting(
 
         # Get input and emission and compute Jacobians
         x, y = inputs[t], emissions[t]
-        current_loss_fn = lambda w: loss_fn(w, x, y)
-        apply_params_fn = lambda w: apply_fn(w, x)
-        g = jacrev(current_loss_fn)(params).squeeze()
-        v = jacrev(apply_params_fn)(params).squeeze()
-        g_tilde = g - _project_to_columns(U, g)
-        v_prime = v - _project_to_columns(U, v)
-        
-        # Update the U matrix
-        u = _normalize(v_prime)
-        U = jnp.where(Sigma.min() > u @ v_prime, U, U.at[:, Sigma.argmin()].set(u))
-        Sigma = jnp.where(Sigma.min() > u @ v_prime, Sigma, Sigma.at[Sigma.argmin()].set(u.T @ v_prime))
 
-        # Update the parameters
-        eta = _stable_division((apply_params_fn(params) - y), (v.T @ g_tilde))
-        params = params - eta * g_tilde
+        # Condition on the emission
+        filtered_params, filtered_U, filtered_Sigma = _orfit_condition_on(params, U, Sigma, loss_fn, apply_fn, x, y)
 
-        return (params, U, Sigma), (params, U)
+        return (filtered_params, filtered_U, filtered_Sigma), (filtered_params, filtered_U)
     
     # Run ORFit
     carry = (initial_mean, U, Sigma)
     _, (filtered_means, filtered_bases) = scan(_step, carry, jnp.arange(len(inputs)))
     return PosteriorORFitFiltered(filtered_means=filtered_means, filtered_bases=filtered_bases)
+
+
+@chex.dataclass
+class ORFitBel:
+    mean: chex.Array
+    basis: chex.Array
+    sigma: chex.Array
+
+
+class RebayesORFit:
+    def __init__(
+        self,
+        orfit_params: ORFitParams,
+    ):
+        self.update_fn = _orfit_condition_on
+        self.mu0 = orfit_params.initial_mean
+        self.m = orfit_params.memory_size
+        self.U0 = jnp.zeros((len(self.mu0), self.m))
+        self.Sigma0 = jnp.zeros((self.m,))
+        self.apply_fn = orfit_params.apply_function
+        self.loss_fn = orfit_params.loss_function
+
+    def initialize(self):
+        return ORFitBel(mean=self.mu0, basis=self.U0, sigma=self.Sigma0)
+
+    def update(self, bel, u, y):
+        m, U, Sigma = bel.mean, bel.basis, bel.sigma # prior predictive for hidden state
+        m_cond, U_cond, Sigma_cond = self.update_fn(m, U, Sigma, self.loss_fn, self.apply_fn, u, y)
+        return ORFitBel(mean=m_cond, basis=U_cond, sigma=Sigma_cond)
+
+    def scan(self, X, Y, callback=None):
+        num_timesteps = X.shape[0]
+        def step(bel, t):
+            bel = self.update(bel, X[t], Y[t])
+            if callback is not None:
+                out = callback(bel, t, X[t], Y[t])
+            return bel, out
+
+        carry = self.initialize()
+        bel, outputs = scan(step, carry, jnp.arange(num_timesteps))
+        return bel, outputs
