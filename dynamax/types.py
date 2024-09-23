42c5c223f08791cc66a8260ac0c875385bc2b6bf
gileshd <gileshd@googlemail.com>
2024-09-23 16:13:07 +0100

    Add IntScalar type

--- dynamax/types.py
+++ dynamax/types.py
@@ -1,6 +1,8 @@
 from typing import Union
-from jaxtyping import Array, Float
+from jaxtyping import Array, Float, Int
 
 PRNGKeyT = Array
 
 Scalar = Union[float, Float[Array, ""]] # python float or scalar jax device array with dtype float
+
+IntScalar = Union[int, Int[Array, ""]]
